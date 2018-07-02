a36ee83600141a0f558b5fd1d97b38d185dcc710
Kamil <kamil.dmytrenko@gmail.com>
2018-07-02 22:16:16 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,110 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Transaction {
+  constructor(fromAddress, toAddress, amount) {
+    this.fromAddress = fromAddress;
+    this.toAddress =toAddress;
+    this.amount = amount;
+  }
+}
+
+class Block {
+  constructor(timestamp, transactions, previousHash = '') {
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.transactions = transactions;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+  calculateHash() {
+    return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
+  }
+  mineBlock(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("BLOCK MINED: " + this.hash);
+  }
+}
+
+class Blockchain{
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 5;
+    this.pendingTransactions = [];
+    this.miningReward = 100;
+  }
+  createGenesisBlock() {
+    return new Block(Date.parse("2017-01-01"), [], "0");
+  }
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+  minePendingTransactions(miningRewardAddress){
+    let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+    block.mineBlock(this.difficulty);
+    this.chain.push(block);
+    this.pendingTransactions = [
+      new Transaction(null, miningRewardAddress, this.miningReward)
+    ];
+  }
+  createTransaction(transaction){
+    this.pendingTransactions.push(transaction);
+  }
+  getBalanceOfAddress(address){
+    let balance = 0;
+
+    for(const block of this.chain){
+      for(const trans of block.transactions){
+        if(trans.fromAddress === address){
+          balance -= trans.amount;
+        }
+
+        if(trans.toAddress === address){
+          balance += trans.amount;
+        }
+      }
+    }
+
+    return balance;
+  }
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++){
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+
+let kamCoin = new Blockchain();
+
+kamCoin.createTransaction(new Transaction('address1', 'address2', 100));
+kamCoin.createTransaction(new Transaction('address2', 'address1', 10));
+
+console.log(`\n Starting the miner...`);
+kamCoin.minePendingTransactions('address1');
+
+console.log(`\n address1 balance is: ${kamCoin.getBalanceOfAddress('address1')}`);
+console.log(`\n address2 balance is: ${kamCoin.getBalanceOfAddress('address2')}`);
+console.log(`\n address3 balance is: ${kamCoin.getBalanceOfAddress('address3')}`);
+
+console.log(`\n Starting the miner...`);
+kamCoin.minePendingTransactions('address3');
+
+console.log(`\n address1 balance is: ${kamCoin.getBalanceOfAddress('address1')}`);
+console.log(`\n address2 balance is: ${kamCoin.getBalanceOfAddress('address2')}`);
+console.log(`\n address3 balance is: ${kamCoin.getBalanceOfAddress('address3')}`);
+
